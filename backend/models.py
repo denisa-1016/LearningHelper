57349361c1c6fa7c8b844b4d388a2b09f6a0c9cf
Soare Denisa <denisasoare1016@gmail.com>
2026-06-21 15:53:47 +0300

    rezolvat

--- backend/models.py
+++ backend/models.py
@@ -11,6 +11,15 @@ class TableContent(Base):
     summary = Column(Text)
     quizJSON = Column(JSON)
 
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {'extend_existing': True}
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
 class Deck(Base):
     __tablename__ = "decks"
 
